33d9810e47da9c5024a19b6e0aa23a723424f005
Andrew Thul <thul.andy@gmail.com>
2014-02-17 15:43:22 -0800

    add rescue exception and '/' path redirect to rulers.rb

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,11 +8,17 @@ module Rulers
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
+      if env['PATH_INFO'] == '/'
+        return [301, {'Content-Type' => 'text/html', 'Location' => '/quotes/a_quote'}, []]
+      end
 
+      begin
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},[text]]
+      rescue Exception[500, {'Content-Type' => 'text/html'}, ['ERROR']]
+      end
     end
   end
 
