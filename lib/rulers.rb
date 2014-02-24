c20d328c44bc9028540e118c2fa69fe781ac683b
Andrew Thul <thul.andy@gmail.com>
2014-02-23 22:17:25 -0800

    ch 4 code

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -15,13 +15,10 @@ module Rulers
         return [301, {'Content-Type' => 'text/html', 'Location' => '/quotes/a_quote'}, []]
       end
 
-      begin
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},[text]]
-      rescue Exception[500, {'Content-Type' => 'text/html'}, ['ERROR']]
-      end
     end
   end
 end
